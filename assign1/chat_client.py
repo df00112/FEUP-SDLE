f20a4c827bfac4b1cd644c1a415e497285ab0a85
Pedro Correia <up202006199@up.pt>
2023-10-09 19:18:36 +0100

    Functioning chat, open the chat_log

--- assign1/chat_client.py
+++ assign1/chat_client.py
@@ -1,32 +1,50 @@
 import zmq
 import threading
 
-username = ""
-subcribed_topics = []
-sub_socket = None
-pub_socket = None
+def receive_messages():
+    while True:
+        message = subscriber.recv_string()
+        
+        message=message.split(" ")
+        topic = message[0]
+        username = message[1]
+        content = ' '.join(message[2:])
+        with open("chat_log.txt", "a") as log_file:
+            log_file.write("User: "+username+" Topic: "+topic+" Message: "+content+"\n")
+        #print(f"Received: {message}")
+
+context = zmq.Context()
+
+# Create a PUB socket for broadcasting messages
+publisher = context.socket(zmq.PUB)
+publisher.connect("tcp://localhost:5560")
+
+# Create a SUB socket for receivingx messages from other clients
+subscriber = context.socket(zmq.SUB)
+subscriber.connect("tcp://localhost:5559")
+subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
+
+# Start a thread to receive messages in the background
+receiver_thread = threading.Thread(target=receive_messages)
+receiver_thread.daemon = True
+receiver_thread.start()
+
+try:
+    while True:
+        message = input("Topic Username Message:")
+        if(message==""):
+            continue
+        message=message.split(" ")
+        topic = message[0]
+        username = message[1]
+        content = ' '.join(message[2:])
+        publisher.send_string(f'{topic} {username} {content}')
+except KeyboardInterrupt:
+    pass
+finally:
+    publisher.close()
+    subscriber.close()
+    context.term()
 
 
-# init handles the initialization of the chat client, asks for username
-def init():
-    print("Welcome to the chat client!")
-    username = input("Please enter your username: ")
-    print("Welcome, " + username + "!")
-
-# Main cycle of the chat client
-def main():
-
-# Asks for input (topic) from the user to subscribe to a topic
-def subscribe_topic():
-
-# Asks for input (topic) from the user to unsubscribe from a topic
-def unsubscribe_topic():
-
-# Asks for which topic the user wants to publish to, then asks for the message
-def pub_side():
-
-# Receives messages from the server and prints them out
-def sub_side():
-
-if __name__ == "__main__":
-    init()
+ 
--- assign1/chat_server.py
+++
@@ -1,9 +0,0 @@
-# chat server
-
-import zmq
-
-
-def chat_server():
-
-if __name__ == "__main__":
-    chat_server()
