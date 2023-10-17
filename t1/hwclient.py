d845f7702a52eda210b082d65889f1a85a4ce56a
Pedro Correia <up202006199@up.pt>
2023-10-17 08:23:24 +0100

    Implemented timeout

--- t1/hwclient.py
+++ t1/hwclient.py
@@ -1,27 +1,32 @@
-#
-#   Hello World client in Python
-#   Connects REQ socket to tcp://localhost:5555
-#   Sends "Hello" to server, expects "World" back
-#
-
 import zmq
+import time
 
 context = zmq.Context()
 
-#  Socket to talk to server
+#  Socket to talk to the server
 print("Connecting to hello world server...")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
-#  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print(f"Sending request {request} ...")
+total_time = 0  # Initialize total time
+
+# Set a timeout (in milliseconds)
+timeout = 5000  # 5 seconds
+
+poller = zmq.Poller()
+poller.register(socket, zmq.POLLIN)
+
+# Loop and accept messages from both channels, acting accordingly
+while True:
     socket.send_string("Hello")
-
-    #  Get the reply.
-    message = socket.recv()
-    print(f"Received reply {request} [ {message} ]")
-
-#  Send kill message to server
-print("Sending request exit ...")
-socket.send_string("exit")
+    print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
+    socks = dict(poller.poll(2000))
+    print("YOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    if socks:
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa")
+        if socks.get(socket) == zmq.POLLIN:
+            print("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+            message = socket.recv_string(zmq.NOBLOCK)
+            print ("got message "+ message)
+    else:
+        print ("error: message timeout")
