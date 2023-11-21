5546039287001f1f5fb8d3a17a81fdcaa440a0f2
df00112 <df00112@hotmail.com>
2023-11-21 08:37:20 +0000

    Imported paranoid

--- project/server.py
+++ project/server.py
@@ -1,6 +1,6 @@
 from random import randint
 import time
-import json
+
 import zmq
 
 HEARTBEAT_LIVENESS = 3
@@ -15,23 +15,13 @@ PPP_HEARTBEAT = b"\x02"  # Signals worker heartbeat
 def worker_socket(context, poller):
     """Helper function that returns a new configured socket
        connected to the Paranoid Pirate queue"""
+    worker = context.socket(zmq.DEALER) # DEALER
     identity = b"%04X-%04X" % (randint(0, 0x10000), randint(0, 0x10000))
     worker.setsockopt(zmq.IDENTITY, identity)
     poller.register(worker, zmq.POLLIN)
     worker.connect("tcp://localhost:5556")
     worker.send(PPP_READY)
     return worker
-
-def get_list(list_name):
-    with open ("./data/cloud/data.json", "r") as f:
-        data = json.load(f)
-    data = json.loads(data)
-    
-    for list in data:
-        if list == list_name:
-            return list
-        
-    return None
 
 
 context = zmq.Context(1)
@@ -53,41 +43,19 @@ while True:
         #  - 3-part envelope + content -> request
         #  - 1-part HEARTBEAT -> heartbeat
         frames = worker.recv_multipart()
-
         if not frames:
             break # Interrupted
 
         if len(frames) == 3:
+            # Simulate various problems, after a few cycles
+            cycles += 1
+            if cycles > 3 and randint(0, 5) == 0:
+                print("I: Simulating a crash")
+                break
+            if cycles > 3 and randint(0, 5) == 0:
+                print("I: Simulating CPU overload")
+                time.sleep(3)
             print("I: Normal reply")
-
-            operation_type = frames[0].decode()
-            client_name = frames[1].decode()
-            data_json = frames[2].decode()
-
-            list_data = json.loads(data_json)
-            if operation_type == "create":
-                print("Creating list")
-            elif operation_type == "update":
-                print("Updating list")
-                # function to get the list from the json file
-                cloud_data = get_list(list_data)
-                #compare the last update date
-                for list in cloud_data:
-                    if list["name"] == list_data["name"]:
-                        if list["lastUpdate"] < list_data["lastUpdate"]:
-                            list["lastUpdate"] = list_data["lastUpdate"]
-                            list["items"] = list_data["items"]
-                            print("List updated")
-                            # save the list to the json file
-                            with open("./data/cloud/data.json", "w") as f:
-                                json.dump(cloud_data, f)
-                                print("List saved")
-                        else:
-                            print("List is already updated")
-
-            elif operation_type == "delete":
-                
-
             worker.send_multipart(frames)
             liveness = HEARTBEAT_LIVENESS
             time.sleep(1)  # Do some heavy work
@@ -115,53 +83,3 @@ while True:
         heartbeat_at = time.time() + HEARTBEAT_INTERVAL
         print("I: Worker heartbeat")
         worker.send(PPP_HEARTBEAT)
-
-
-
-
-
-""" import zmq
-
-context = zmq.Context()
-
-# publisher socket handles the server's heartbeats
-publisher = context.socket(zmq.PUB)
-publisher.bind("tcp://127.0.0.1:5556")
-
-# rep socket handles the main server-client communication - authentication, retrieve sub server
-server = context.socket(zmq.REP)
-server.bind("tcp://127.0.0.1:5557")
-
-shopping_list = []
-
-while True:
-    try:
-        # Handle client edit requests using REQ-REP
-        edit_request = server.recv_json()
-        
-        action = edit_request["action"]
-        item = edit_request.get("item")
-
-        if action == "add":
-            if item:
-                shopping_list.append(item)
-                update_message = f"Item added: {item}"
-                publisher.send_string(update_message)
-
-        elif action == "delete":
-            if item in shopping_list:
-                shopping_list.remove(item)
-                update_message = f"Item deleted: {item}"
-                publisher.send_string(update_message)
-
-        elif action == "authentication":
-            
-            
-
-    except KeyboardInterrupt:
-        break
-
-publisher.close()
-server.close()
-context.term()
- """
